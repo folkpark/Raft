65aab0d08c9ad82853690402ea0d3c3c499428d8
folkpark <parkerfolkman@gmail.com>
2018-03-30 16:24:40 -0600

    Working on REQ/REP functionality.

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ port = "5050"
 context = zmq.Context()
 socket = context.socket(zmq.PAIR)
 time.sleep(2)
-socket.bind("tcp://10.142.0.8:%s" % port)
 
 while True:
+    socket.connect("tcp://10.142.0.8:%s" % port)
     socket.send_string("Server message to client3")
     msg = socket.recv()
     print(msg)
