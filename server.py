453811cedf97f9dddeae9c28c579e58e99bd1958
folkpark <parkerfolkman@gmail.com>
2018-03-31 11:27:04 -0600

    Working on REQ/REP functionality.

--- server.py
+++ server.py
@@ -11,11 +11,12 @@ socket = context.socket(zmq.PAIR)
 socket.bind("tcp://10.142.0.4:%s" % port)
 
 socket2 = context.socket(zmq.PAIR)
-socket.bind("tcp://10.142.0.4:%s" % port2)
+socket2.bind("tcp://10.142.0.4:%s" % port2)
 
 while True:
     p = pickle.dumps("Server message to client3")
     socket.send(p)
+    socket2.send(p)
     msg = socket.recv()
     pmessage = pickle.loads(msg)
     print(pmessage)
