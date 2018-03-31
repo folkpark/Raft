7f46755061f5514275997c0284093fde02c23e68
folkpark <parkerfolkman@gmail.com>
2018-03-31 10:55:49 -0600

    Working on REQ/REP functionality.

---
+++ client2.py
@@ -0,0 +1,20 @@
+import zmq
+import pickle
+import random
+import sys
+import time
+
+port = "5050"
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
+socket.connect("tcp://10.142.0.3:%s" % port)
+
+while True:
+    p = pickle.dumps("client message to server1")
+    p2 = pickle.dumps("client message to server2")
+    msg = socket.recv()
+    pmessage = pickle.loads(msg)
+    print(pmessage)
+    socket.send(p)
+    socket.send(p2)
+    time.sleep(1)
